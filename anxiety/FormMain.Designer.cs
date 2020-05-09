f869c58bc1e71ebe178626442a561b64dda78aac
thisita <ezebeezee@gmail.com>
2020-05-09 17:34:42 -0400

    import

---
+++ anxiety/FormMain.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace anxiety
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.LabelWatching = new System.Windows.Forms.Label();
+            this.TimerWarning = new System.Windows.Forms.Timer(this.components);
+            this.TimerMoveEyes = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // LabelWatching
+            // 
+            this.LabelWatching.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LabelWatching.Location = new System.Drawing.Point(0, 0);
+            this.LabelWatching.Name = "LabelWatching";
+            this.LabelWatching.Size = new System.Drawing.Size(253, 76);
+            this.LabelWatching.TabIndex = 0;
+            this.LabelWatching.Text = resources.GetString("LabelWatching.Text");
+            this.LabelWatching.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TimerWarning
+            // 
+            this.TimerWarning.Enabled = true;
+            this.TimerWarning.Tick += new System.EventHandler(this.TimerWarning_Tick);
+            // 
+            // TimerMoveEyes
+            // 
+            this.TimerMoveEyes.Enabled = true;
+            this.TimerMoveEyes.Tick += new System.EventHandler(this.TimerMoveEyes_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(253, 76);
+            this.ControlBox = false;
+            this.Controls.Add(this.LabelWatching);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormMain";
+            this.Opacity = 0.03D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelWatching;
+        private System.Windows.Forms.Timer TimerWarning;
+        private System.Windows.Forms.Timer TimerMoveEyes;
+    }
+}
+
---
+++ anxiety/FormMain.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace anxiety
+{
+    public partial class FormMain : Form
+    {
+        private readonly Random random;
+        public FormMain()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        private void DisplayWarning()
+        {
+            MessageBox.Show("", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+        }
+
+        private void SetTimerWarningInterval()
+        {
+            int type = random.Next(3);
+            int interval;
+            switch(type)
+            {
+                case 2:
+                    interval = random.Next(300, 1000);
+                    break;
+                case 3:
+                    interval = random.Next(1000, 60000);
+                    break;
+                case 0:
+                case 1:
+                default:
+                    interval = random.Next(15, 300);
+                    break;
+            }
+            TimerWarning.Interval = interval * 100;
+        }
+
+        private void TimerWarning_Tick(object sender, EventArgs e)
+        {
+            TimerWarning.Enabled = false;
+            TimerWarning.Stop();
+            DisplayWarning();
+            SetTimerWarningInterval();
+            TimerWarning.Enabled = true;
+            TimerWarning.Start();
+        }
+
+        private void TimerMoveEyes_Tick(object sender, EventArgs e)
+        {
+            TimerMoveEyes.Enabled = false;
+            TimerMoveEyes.Stop();
+            int x = random.Next(SystemInformation.VirtualScreen.Width);
+            int y = random.Next(SystemInformation.VirtualScreen.Height);
+            Location = new Point(x, y);
+            TimerMoveEyes.Interval = random.Next(15, 300) * 100;
+            TimerMoveEyes.Enabled = true;
+            TimerMoveEyes.Start();
+        }
+    }
+}
